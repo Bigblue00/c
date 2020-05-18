d52b905060293101525b168b8c79c1118d39fd52
nanda <33324450+Bigblue00@users.noreply.github.com>
2020-05-18 22:42:19 +0530

    Update linearsearch.c

--- linearsearch.c
+++ linearsearch.c
@@ -1,20 +1,23 @@
 #include<stdio.h>
 int main()
 {
-int n,x,a[100],count=0,i;
-scanf("%d",&n);
-
-for(i=0;i<n;i++)
-scanf("%d",&a[i]);
-
-scanf("%d",&x);
-for(i=0;i<n;i++)
-if(a[i]==x)
-count++;
-
-if(count>0)
-printf("found");
-else
-printf("not found")
-return 0;
+    int i,a[100],x,n,count=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++){
+    scanf("%d",&a[i]);
+    }
+    scanf("%d",&x);
+    for(i=0;i<n;i++)
+    {
+        if(a[i]==x)
+        {
+            count++;
+            if(count>0)
+            {   
+            printf("%d is present %d times at %d\n",x,count,i+1);
+            }
+        }
+    }
+    
+    return 0;
 }
